d44c027801fa77aa44fd069070408d47024f05b9
rajesh-rkb <bagderajesh68@gmail.com>
2025-01-03 23:09:33 +0530

    added extra line in app.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
     <div className="App" id="root">
       <MyNavbar />
       
+      
       <Routes>
         <Route path="/products" element={<Products />} />
         <Route path="/customers" element={<Customers />} />
